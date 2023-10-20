d2d69182f3f58aea72f4260fa5021f630d27fa41
NarendranathV13 <narendranath.v@arkinfotec.com>
2023-10-20 19:50:16 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-
 import './App.css';
-
-// import Dummy2 from './pages/Dummy/Dummy2';
 import AdminPage from './pages/AdminPage/AdminPage';
 
 function App() {
   return (
     <div className="App">
       <AdminPage />
-     
-      {/* <Dummy2 /> */}
     </div>
   );
 }
